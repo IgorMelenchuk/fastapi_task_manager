c89a96448fdf0066c9aaf7bcf5ea34cd1ce91746
IgorMelenchuk <igormelenchuk1@gmail.com>
2024-12-24 00:00:27 +0300

    Update api_healthcheck module

--- api_healthcheck.py
+++ api_healthcheck.py
@@ -4,24 +4,24 @@ from base64 import urlsafe_b64encode
 API_KEY = ""
 API_URL = "https://www.virustotal.com/api/v3/urls"
 
-# Пример: проверка URL
+
 def check_url_safety(url_to_check):
-    # Шаг 1: Закодировать URL в base64
+
     url_id = urlsafe_b64encode(url_to_check.encode()).decode().strip("=")
 
-    # Шаг 2: Отправить GET-запрос с закодированным URL
+
     headers = {
-        "x-apikey": API_KEY  # Убедитесь, что ключ передается корректно
+        "x-apikey": API_KEY
     }
     response = requests.get(f"{API_URL}/{url_id}", headers=headers)
 
-    # Шаг 3: Отладочная информация
+
     print("Request URL:", f"{API_URL}/{url_id}")
     print("Headers:", headers)
     print("Status Code:", response.status_code)
     print("Response Text:", response.text)
 
-    # Шаг 4: Вернуть JSON-ответ или обработать ошибку
+
     if response.status_code == 200:
         return response.json()
     else:
